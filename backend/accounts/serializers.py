d242606ca50814e3200fc3d517298817abae79e8
iambhaveshrajput <bhaveshrajput4321@gmail.com>
2026-02-20 21:16:27 +0530

    Update serializers.py

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -42,10 +42,10 @@ class RegisterSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
+            password= password,
             first_name=validated_data.get('first_name', ''),
             last_name=validated_data.get('last_name', '')
         )
-        user.set_password(validated_data['password'])
         user.save()
         return user
 
