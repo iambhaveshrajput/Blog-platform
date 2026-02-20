8048efba294759f0b85bd15092af00f518e94aba
iambhaveshrajput <bhaveshrajput4321@gmail.com>
2026-02-20 20:27:27 +0530

    Update settings.py

--- backend/blogproject/settings.py
+++ backend/blogproject/settings.py
@@ -114,8 +114,8 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-    'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
